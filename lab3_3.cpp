d69d0b7f2ae1014629632e52d09f1144a880b7c9
lab302-23 <lab302-23@labeng.local>
2019-01-11 16:17:20 +0700

    version 2-69

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -1,1 +1,15 @@
 #include <iostream>
+
+using namespace std;
+int main(){
+    int a = 2;
+    double sum = 0;
+
+    while(a<=69){
+        sum = 1.0/a + sum;
+        a++;
+    }
+    cout << sum;
+    return 0;
+}
+
