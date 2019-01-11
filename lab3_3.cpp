ebf87ac803aeb314f5b22cfa3316c294593ddbcb
lab302-23 <lab302-23@labeng.local>
2019-01-11 16:58:05 +0700

    Question 3

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -2,22 +2,16 @@
 
 using namespace std;
 int main(){
-   string text1,text2;
-   int N;
-   int count = 0;
-   cout << "Enter the first text ";
-    cin >> text1;
-   cout << "Enter the second text ";
-    cin >> text2;
-   cout << "Enter N: ";
-    cin >> N;
-    while(count < N){
-        if(count % 2 == 0)
-            cout << text1 ;
-        else 
-            cout << text2 ;
-        count++;
+    int a = 2;
+    double sum = 0;
+
+    while(a<=69){
+        sum = 1.0/a + sum;
+        a++;
     }
+    cout << sum;
+    return 0;
+}
    
 
 }
